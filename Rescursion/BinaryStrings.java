e599dcf7c25a1bc86cb2681be360255f1a03d502
Zishan Pathan <141132499+Zishan-Khan01@users.noreply.github.com>
2026-07-26 15:55:31 +0530

    Create BinaryStrings.java

---
+++ Rescursion/BinaryStrings.java
@@ -0,0 +1,15 @@
+class BinaryStrings{
+	public static void binaryStrings(int n, int lastPlace, String str){
+		if(n == 0){
+			System.out.println(str);
+			return;
+		}
+		binaryStrings(n-1, 0, str+"0");
+		if(lastPlace == 0){
+			binaryStrings(n-1, 1, str+"1");
+		}
+	}
+	public static void main(String args[]){
+		binaryStrings(3, 0, "");
+	}
+}
