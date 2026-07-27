46c4a90acccdec301b5436d6e197bcb2fcf36434
Zishan Pathan <141132499+Zishan-Khan01@users.noreply.github.com>
2026-07-27 18:02:25 +0530

    Add files via upload

---
+++ Rescursion/FactorialOfN.java
@@ -0,0 +1,16 @@
+class FactorialOfN{
+	public static int fact(int n){
+		if(n==0){
+			return 1;
+		}
+		//int fnm1 = fact(n-1);  //fnm = fact(n-1)
+		int fn = n * fact(n-1);  //fn = fact(n)
+		return fn;
+	}
+
+
+	public static void main(String args[]){
+		int n = 5;
+		System.out.println(fact(n));
+	}
+}
